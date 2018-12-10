38b543ba87a7b8b489c3eb654233692178ac5c0c
Pramodh Arya <pramodharya@gmail.com>
2018-12-10 11:03:11 -0500

    ite

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNoCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNoCrater.java
@@ -28,7 +28,7 @@ public class AutoNoCrater extends LinearOpMode {
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    
+
     static final double     DRIVE_SPEED             = 1;     // Nominal speed for better accuracy.
     static final double     TURN_SPEED              = 0.7;     // Nominal half speed for better accuracy.
 
